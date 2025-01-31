a4f079154db00a08f7ddab2115b0cb4f5fae39f2
hridaya <hridayamishra225@gmail.com>
2025-01-31 10:13:25 +0545

    Pagination and sessions

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path,include
+from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index, name='index'),
-    path('books', views.BookListView.as_view(), name='book-list') #Class Based Views 
+    path('books', views.BookListView.as_view(), name='book-list'), #Class Based Views 
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail') #Class Based Views 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -32,10 +32,41 @@ class BookListView(generic.ListView): #GET METHOD
   '''This will query our database and get all the records of the books that are available and it will render the records in our book_list template it will assume it is present in our templates/catalog(appname)/book_list.html(model_list)'''
   model= Book
   # Context default name--> book_list -> (modelName)_list
-  template_name = 'book_list.html'
-
+  template_name = 'book_list.html' #templates->catalog(appname)->book_list.html
   # def get_queryset(self):
   #   super().get_queryset()
-    
+  paginate_by= 1
+  # Data 
   # context_object_name = 'book_list'
 
+class BookDetailView(generic.DetailView):
+  '''Detail view is the class based generic view: Single book ko detail information present garxa.. 
+  1. ID Pathaunu parxa ? 
+  2. model 
+  3. template name over write
+  4. context_object_name --> book_detail ()
+  5. Custom query pani patahunu sakxam --> 
+  '''
+  model = Book
+  template_name ='book_detail.html'
+  # context_object_name = 'book'
+  
+  #TODO: REquest Sessions -id match book return context 
+  # HARRAY POTER, MACBETH, FIRE
+  # MACBETH
+  def get_context_data(self, **kwargs): #--> Dictionary, List, Memory variable member reference
+    context= super().get_context_data(**kwargs)
+    book = self.get_object()
+    # recently_viewed_books = []
+    if 'recently_viewed' in self.request.session: # check gareko already books haru addd garko xa ki xaina 
+      # Recently_viewed array yo self.reqyest.session dictionary--> book.pk check garnae 
+      if book.pk not in self.request.session['recently_viewed']:
+        self.request.session['recently_viewed'].insert(0, book.pk)
+      if len(self.request.session['recently_viewed']) > 3:
+        self.request.session['recently_viewed'].pop()
+    else: # first xoti add garnu khojeko 
+      self.request.session['recently_viewed'] = [book.pk]
+    self.request.session.modified = True
+    recently_viewed_books_id = self.request.session['recently_viewed']
+    context['recently_viewed'] =  Book.objects.filter(pk__in=recently_viewed_books_id)
+    return context
